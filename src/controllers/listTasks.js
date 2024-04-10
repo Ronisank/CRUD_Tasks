a8966a334cf1cfe37b8935ad43f5b1515ae3b5ae
Roni <ronisank21@hotmail.com>
2024-04-09 22:45:34 -0300

    add pagination

--- src/controllers/listTasks.js
+++ src/controllers/listTasks.js
@@ -4,7 +4,28 @@ const {tasks} = require("../utils/tasks");
 const listTasks = (req, res) =>{
     const task = tasks;
     try {
-        res.status(200).send(task);
+        //Página atual
+        const {page = 1} = req.query;
+        console.log(page);
+
+        //Limite de tarefas por página
+        const limit = 3;
+//Calculo para exibir as tarefas de acordo com a página
+        lastPage = 1
+
+        const countTasks = task.length;
+        console.log(countTasks);
+//Calculo para exibir as tarefas de acordo com a página
+if (countTasks != 0){
+    lastPage = Math.ceil(countTasks / limit);
+    res.status(200).send(task);
+
+    console.log(lastPage);
+}else{
+    return res.status(400).json({ mensage: "Não há tarefas cadastradas"});
+    
+}
+
 
     } catch (error) {
         res.status(400).send(`catch do list - ${error}`);
